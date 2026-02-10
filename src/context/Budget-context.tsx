ca9a74b1955c0da488c9f9fed507bf42b1beceab
Cherry-Machado <cherry5467@gmail.com>
2026-02-10 17:48:35 -0300

    Implementing Context API

---
+++ src/context/Budget-context.tsx
@@ -0,0 +1,30 @@
+import { useReducer, createContext } from "react";
+import { type Dispatch, type ReactNode } from "react";
+import { type BudgetActions, type BudgetState, budgetReducer, initiateSate } from "../reducers/budget-reducer";
+
+type BudgetContextProps = {
+  state: BudgetState;
+  dispatch: Dispatch<BudgetActions>; 
+}
+
+type BudgetProviderProps = {
+  children: ReactNode;
+}
+
+export const BudgetContext = createContext<BudgetContextProps>(null!);
+
+export const BudgetProvider = ({children} : BudgetProviderProps) => {
+
+    const [state, dispatch] = useReducer(budgetReducer, initiateSate);
+
+    return (
+        <BudgetContext.Provider
+            value={{
+                state,
+                dispatch
+            }}
+        >
+            {children}
+        </BudgetContext.Provider>
+    )
+}
