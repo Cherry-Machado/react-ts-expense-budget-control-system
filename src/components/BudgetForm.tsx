483e784e8fe9c9f0d4cfef0223a967bf5b9a4474
Cherry-Machado <cherry5467@gmail.com>
2026-02-10 16:31:34 -0300

    Updating buget form

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+
 export default function BudgetForm() {
+
+  const [budget, setBudget] = useState(0);
+
+  const handleBudgetChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBudget(Number(e.target.value));
+    console.log(budget);
+  }
+
   return (
-    <div>BudgetForm</div>
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+          Set Budget
+        </label>
+        <input
+          type="number"
+          id="budget"
+          name="budget"
+          className="w-full bg-white border border-gray-200 p-2"
+          value={budget}
+          onChange={handleBudgetChange}
+          placeholder="Set your budget amount"
+        />
+      </div>
+
+      <input 
+        type="submit"
+        value="Set Budget"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase" 
+      />
+
+    </form> 
   )
 }
