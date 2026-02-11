f8bdb0af0bdc4fb516c905ab4590be05c9fbab39
Cherry-Machado <cherry5467@gmail.com>
2026-02-11 01:10:53 -0300

    calling hook from Budget form

--- src/hooks/useBudget.ts
+++ src/hooks/useBudget.ts
@@ -1,8 +1,12 @@
 // Creating hook to easily access the budget context in any component
+
 import { useContext } from "react";
 import { BudgetContext } from "../context/BudgetContext";
 
 export const useBudget = () => {
     const context = useContext(BudgetContext);
+    if(!context) {
+        throw new Error('useBudget must be used within a BudgetProvider');
+    }
     return context;
 }
