c78f403e5c41ad2ab6b0ece80a74caf1a3bb2ca6
Cherry-Machado <cherry5467@gmail.com>
2026-02-12 15:17:31 -0300

    Showing icons of the expense category

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,19 +1,28 @@
 // This component will be used to show the details of an expense when the user clicks on an expense in the list. It will show the name, amount, date, and category of the expense. It will also have a button to delete the expense and a button to edit the expense.
+import { use, useMemo } from "react"
 import { formatDate } from "../helpers"
 import type { Expense} from "../types"
 import AmountDisplay from "./AmountDisplay"
+import { categories } from "../data/categories"
 
 type ExpenseDetailProps = {
     expense: Expense
 }
 export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
 
+    const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense]);
   return (
     <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
         <div>
-
+            <img 
+                src={`/icono_${categoryInfo.icon}.svg`} 
+                alt={`Icon of ${categoryInfo.name}`} 
+                className="w-20" 
+            />
         </div>
-        <div>
+
+        <div className="flex-1 space-y-2">
+            <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
             <p>{expense.expenseName}</p>
             <p className="text-slate-600 text-sm">{ formatDate(expense.date!.toString() || '')}</p>
         </div>
