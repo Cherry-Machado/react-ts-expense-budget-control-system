58a7e73ef883e5d4b4910f45dd80de6c1032cfd8
Cherry-Machado <cherry5467@gmail.com>
2026-02-13 10:02:25 -0300

    Creating filter by category

---
+++ src/components/FilterByCategory.tsx
@@ -0,0 +1,31 @@
+
+export default function FilterByCategory() {
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-10">
+        <form>
+            <div className="flex flex-col md:flex-row md:items-center gap-5">
+                <label 
+                    htmlFor="category"
+                    className="text-xl"
+                >
+                    Filter by Category:
+                </label>
+                <select 
+                    id="category"
+                    className="bg-slate-100 p-3 flex-1 rounded"
+                >
+                        <option value="">-- Select a category --</option>
+                        {categories.map((category) => (
+                            <option 
+                                value={category.id}
+                                 key={category.id}
+                            >
+                                {category.name}
+                            </option>
+                        ))}
+                </select>
+            </div>
+        </form>
+    </div>
+  )
+}
